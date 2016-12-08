e35d656d3f0a7089edf017814793e6d000ce97d2
kosowina <vivanesca@gmail.com>
2016-12-08 08:17:47 +0000

    device install

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,9 @@
 Rails.application.routes.draw do
-  get 'meals/show'
-
-  get 'meals/new'
-
-  get 'meals/create'
-
-  get 'meals/edit'
-
-  get 'meals/destroy'
-
-  get 'register/registration'
-
-  get 'static/about'
-
-  get 'static/contact'
+  root 'static_pages#home'
+  get '/about', to: 'static#about'
+  get '/contact', to: 'static#contact'
+  resources :reserve
+  resources :meals
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
