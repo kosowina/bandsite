854ccf9f53724271830e45dc96a4701f07b2884b
kosowina <vivanesca@gmail.com>
2016-12-13 22:30:18 +0000

    Seedy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tag.create!(name: "wegetariańskie")
+Tag.create!(name: "kurczak")
+Tag.create!(name: "kaczka")
+Tag.create!(name: "ryż")
+Tag.create!(name: "makaron")
+Tag.create!(name: "wołowina")
+Tag.create!(name: "wieprzowina")
+Tag.create!(name: "cielęcina")
+Tag.create!(name: "owoce morza")
+Tag.create!(name: "ryby")
+
+Category.create!(name: "przystawki")
+Category.create!(name: "dania główne")
+Category.create!(name: "dodatki")
+Category.create!(name: "zupy")
+Category.create!(name: "desery")
+
+Table.create!(capacity: "4")
+Table.create!(capacity: "4")
+Table.create!(capacity: "4")
+Table.create!(capacity: "4")
+Table.create!(capacity: "4")
+Table.create!(capacity: "2")
+Table.create!(capacity: "2")
+Table.create!(capacity: "2")
+Table.create!(capacity: "2")
+
+
+
+
+
