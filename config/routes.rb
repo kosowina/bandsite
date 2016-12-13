ece7b5da59ff3554eb800f60557dfacca7000dcd
kosowina <vivanesca@gmail.com>
2016-12-13 19:49:04 +0000

    Statyczne strony i baza danych

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'application#hello'  
+  
+  root 'static#about'
   get '/about', to: 'static#about'
   get '/home', to: 'static#home'
   get '/contact', to: 'static#contact'
   resources :reserve
   resources :meals
-
+  devise_for :users
 end
