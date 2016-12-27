08685e417a9f07c181af7cace3329ab362491512
kosowina <vivanesca@gmail.com>
2016-12-27 18:23:58 +0000

    dziala

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,14 +2,16 @@ class Reservation < ApplicationRecord
   belongs_to :table
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
+  validates :email, length: { maximum: 255 }, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {conditions: -> { 
+                                 where("DATE(created_at) = ?", Date.today) } }
   validates :username, presence: true, length: { maximum: 50 } 
   validates :phone, presence: true, numericality: true, length: {is: 9}
   validates :table_id, presence: true
-  validates :hour, presence: true, length: { minimum: 6, maximum: 24 } 
-  validates_uniqueness_of :hour, :email,
-    conditions: -> { where("DATE(created_at) = ?", Date.today) } 
-  validates :table_id, :uniqueness => {:scope => :hour}
+  validates :hour, presence: true, numericality:
+                   { :greater_or_equal_than => 6, :less_than => 24 }, 
+                   uniqueness: {:scope => :table_id, conditions: -> { 
+                                where("DATE(created_at) = ?", Date.today) } }
     
 end
